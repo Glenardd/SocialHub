df9f2ab3d283b43f2bb7071b9f99676a5f0e1dec
Glenardd <tolentinojanglenard5@gmail.com>
2024-06-22 03:22:51 +0800

    Search not found added

--- src/app/account/user/result.tsx
+++ src/app/account/user/result.tsx
@@ -3,7 +3,7 @@ import useSWR from "swr";
 import Link from 'next/link';
 import { getCookie } from "typescript-cookie";
 
-export default function result({searchResult}:any) {
+export default function Result({searchResult}:any) {
   
   const fetcher = (...args: [RequestInfo, RequestInit]) => fetch(...args).then((res) => res.json());
 
@@ -13,26 +13,36 @@ export default function result({searchResult}:any) {
 
   const userLogged = getCookie('isLogged');
 
+  const hasResults = userAccounts?.some((acc: any) =>
+    acc?.username.toLowerCase().startsWith(searchResult?.toLowerCase())
+  );
+
   return (
     <>
-        <ul>
-          {
-            userAccounts?.map((acc:any)=>{
+        {
+          hasResults ? (
+            <ul>
+              {
+                userAccounts?.map((acc:any)=>{
 
-              const username = acc?.username;
+                  const username = acc?.username;
 
-              const caseSensitiveCheck = acc?.username.toLowerCase().startsWith(searchResult?.toLowerCase());
+                  const caseSensitiveCheck = acc?.username.toLowerCase().startsWith(searchResult?.toLowerCase());
 
-              const isUserLogged = acc?.username.toLowerCase().startsWith(userLogged?.toLowerCase());
-              
-              if(caseSensitiveCheck){
-                return (
-                  <li><Link href={ isUserLogged ? "/account" : `/account/${username}`}>{username}{isUserLogged ? '(you)': ''}</Link></li>
-                )
-              }
-            })
-          }          
-        </ul>
+                  const isUserLogged = acc?.username.toLowerCase().startsWith(userLogged?.toLowerCase());
+
+                  if(caseSensitiveCheck){
+                    return (
+                      <li><Link href={ isUserLogged ? "/account" : `/account/${username}`}>{username}{isUserLogged ? '(you)': ''}</Link></li>
+                    )
+                  }
+                })
+              }          
+            </ul>
+          ) : (
+            <div>Not found</div>
+          )
+        }
         
     </>
   )
