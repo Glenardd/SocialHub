4b2d17e0a49d13f6e894e798824ea0869c4358aa
Glenardd <tolentinojanglenard5@gmail.com>
2024-06-22 04:18:46 +0800

    search "not found" message fixed

--- src/app/account/user/result.tsx
+++ src/app/account/user/result.tsx
@@ -2,6 +2,7 @@
 import useSWR from "swr";
 import Link from 'next/link';
 import { getCookie } from "typescript-cookie";
+import { usePathname } from "next/navigation";
 
 export default function Result({searchResult}:any) {
   
@@ -13,37 +14,40 @@ export default function Result({searchResult}:any) {
 
   const userLogged = getCookie('isLogged');
 
-  const hasResults = userAccounts?.some((acc: any) =>
-    acc?.username.toLowerCase().startsWith(searchResult?.toLowerCase())
-  );
+  const resultsPath = usePathname();
+
+  const hasResults = userAccounts?.some((acc: any) => {
+    return acc?.username.toLowerCase().startsWith(searchResult?.toLowerCase());
+  });
+
+  //to stop showing "Not found" in the /account route
+  const userResultsPath = resultsPath === "/account/user" ? true : false;
 
   return (
     <>
+      <ul>
         {
-          hasResults ? (
-            <ul>
-              {
-                userAccounts?.map((acc:any)=>{
+          userAccounts?.map((acc:any)=>{
 
-                  const username = acc?.username;
+            const username = acc?.username;
 
-                  const caseSensitiveCheck = acc?.username.toLowerCase().startsWith(searchResult?.toLowerCase());
+            const caseSensitiveCheck = acc?.username.toLowerCase().startsWith(searchResult?.toLowerCase());
 
-                  const isUserLogged = acc?.username.toLowerCase().startsWith(userLogged?.toLowerCase());
+            const isUserLogged = acc?.username.toLowerCase().startsWith(userLogged?.toLowerCase());
 
-                  if(caseSensitiveCheck){
-                    return (
-                      <li><Link href={ isUserLogged ? "/account" : `/account/${username}`}>{username}{isUserLogged ? '(you)': ''}</Link></li>
-                    )
-                  }
-                })
-              }          
-            </ul>
-          ) : (
-            <div>Not found</div>
-          )
-        }
-        
+            if(caseSensitiveCheck){
+              return (
+                <li><Link href={ isUserLogged ? "/account" : `/account/${username}`}>{username}{isUserLogged ? '(you)': ''}</Link></li>
+              )
+            }
+          })
+        }          
+      </ul>
+      {
+        //Will only show if in the path of /account/user
+        //and will show only if search results were not found
+        userResultsPath && !hasResults && <div>Not found</div>
+      }
     </>
   )
 }
