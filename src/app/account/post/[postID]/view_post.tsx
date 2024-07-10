3f2bbb67b38159a1f1a0a2bc373cb08d939e37e4
Glenardd <tolentinojanglenard5@gmail.com>
2024-07-11 02:10:25 +0800

    updated liking and disliking post in view post page of main logged user

--- src/app/account/post/[postID]/view_post.tsx
+++ src/app/account/post/[postID]/view_post.tsx
@@ -34,13 +34,18 @@ export default function view_post() {
   const foundPost = postData?.find((post:any)=> post?.id === postId);
 
   const postTextMessage = foundPost?.text_message;
-  const postOwner = foundPost?.user;
+  
+  const currentLoggedUserID = foundPost?.user;
   const postCreated = foundPost?.created;
-  const postLikes = foundPost?.likes;
-  const postDislikes = foundPost?.dislikes;
+  
+  const usersWhoLikeNum = foundPost?.user_likes?.length;
+  const usersWhoDislikeNum = foundPost?.user_dislikes?.length;
+
+  const usersWhoLike = foundPost?.user_likes;
+  const usersWhoDislike = foundPost?.user_dislikes;
 
   //get the username of the post user
-  const accountOwner = user?.find((acc:any)=>acc?.id === postOwner)?.username;
+  const currentLoggedUserUsername = user?.find((acc:any)=>acc?.id === currentLoggedUserID)?.username;
 
   //time format
   const formatCreatedTime = (created: string) => {
@@ -73,55 +78,136 @@ export default function view_post() {
 };
 
   //likes
-  const handleLikes = async (postId:any) =>{
-    
+  const handleLikes = async (postId: any) => {
+    //checks if the current user already like the post
+    const hasLiked = () =>{ 
+        //checks if user is in user_likes
+        const checkUser = usersWhoLike?.includes(currentLoggedUserID);
+
+        //if the user is inside the user_likes remove it when clicked again
+        if(checkUser){
+            const removeLikeUser = usersWhoLike?.filter((userId:any)=> userId !== currentLoggedUserID);
+            
+            return removeLikeUser;
+        //add the logged user if still not inside the user_likes
+        }else{
+            const addLikeUser = [...usersWhoLike, currentLoggedUserID];
+            
+            return addLikeUser;
+        };
+    };
+
     const likeData = {
-      ...postLikes,
-      "likes": postLikes+1,
+        "user_likes": hasLiked(),
     };
 
-    const like = await fetch(`http://127.0.0.1:8090/api/collections/status_update/records/${postId}`, {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(likeData),
-    });
+    try{
+        const like = await fetch(`http://127.0.0.1:8090/api/collections/status_update/records/${postId}`,{
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(likeData),
+        });
 
-    if(like.ok){
-      mutate("http://127.0.0.1:8090/api/collections/status_update/records");
-    };
+        if(like.ok){
+            const removeUser = usersWhoDislike?.filter((userId:any)=> userId !== currentLoggedUserID);
+
+            const dislikeData = {
+                "user_dislikes": removeUser,
+            };
+
+            try{
+                const removeFromDislike = await fetch(`http://127.0.0.1:8090/api/collections/status_update/records/${postId}`,{
+                    method: "PATCH",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(dislikeData),
+                });
+
+                if(removeFromDislike.ok){
+                    mutate("http://127.0.0.1:8090/api/collections/status_update/records");
+                }
+            }catch(error){
+                console.log(error);
+            };
+        };
+    }catch(error){
+        console.log(error);
+    };  
   };
 
   //dislikes
   const handleDislikes = async (postId:any) =>{
-    
-    const dislikeData = {
-      ...postDislikes,
-      "dislikes": postDislikes+1,
+    //checks if the current user already like the post
+    const hasDisliked = () =>{ 
+        //checks if user is in user_likes
+        const checkUser = usersWhoDislike?.includes(currentLoggedUserID);
+
+        //if the user is inside the user_likes remove it when clicked again
+        if(checkUser){
+            const removeDislikeUser = usersWhoDislike?.filter((userId:any)=> userId !== currentLoggedUserID);
+            
+            return removeDislikeUser;
+        //add the logged user if still not inside the user_likes
+        }else{
+            const addLikeUser = [...usersWhoLike, currentLoggedUserID];
+            
+            return addLikeUser;
+        };
     };
 
-    const dislike = await fetch(`http://127.0.0.1:8090/api/collections/status_update/records/${postId}`, {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(dislikeData),
-    });
+    const likeData = {
+        "user_dislikes": hasDisliked(),
+    };
 
-    if(dislike.ok){
-      mutate("http://127.0.0.1:8090/api/collections/status_update/records");
+    try{
+        const like = await fetch(`http://127.0.0.1:8090/api/collections/status_update/records/${postId}`,{
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(likeData),
+        });
+
+        if(like.ok){
+            const removeUser = usersWhoLike?.filter((userId:any)=> userId !== currentLoggedUserID);
+
+            const dislikeData = {
+                "user_likes": removeUser,
+            };
+
+            try{
+                const removeFromLike = await fetch(`http://127.0.0.1:8090/api/collections/status_update/records/${postId}`,{
+                    method: "PATCH",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(dislikeData),
+                });
+
+                if(removeFromLike.ok){
+                    mutate("http://127.0.0.1:8090/api/collections/status_update/records");
+                }
+            }catch(error){
+                console.log(error);
+            };
+        };
+    }catch(error){
+        console.log(error);
     };
   };
 
-  const loggedUser = accountOwner === userLogged ? "(You)" : "";
+
+  const loggedUser = currentLoggedUserUsername === userLogged ? "(You)" : "";
 
   return (
     <>
-      <h2><Link href={loggedUser ? "/account":""}>{accountOwner}{loggedUser}</Link></h2>
+      <h2><Link href={loggedUser ? "/account":""}>{currentLoggedUserUsername}{loggedUser}</Link></h2>
       <h2>Posted: {formatCreatedTime(postCreated)}</h2>
       <h2>{postTextMessage}</h2>
-      <h2>Likes: {postLikes} Dislikes: {postDislikes}</h2>
+      <h2>Likes: {usersWhoLikeNum} Dislikes: {usersWhoDislikeNum}</h2>
       <button onClick={()=> handleLikes(postId)}>like</button>
       <button onClick={()=> handleDislikes(postId)}>dislike</button>
     </>
